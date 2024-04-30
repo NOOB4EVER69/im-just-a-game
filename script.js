90cf9cc6cc997642b28d639f7cd0c0c16150e1d0
Mustapha <124893828+NOOB4EVER69@users.noreply.github.com>
2024-05-01 00:47:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+let score = 0;
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 's') {
+        score++;
+        updateScore();
+        checkAchievements();
+    }
+});
+
+function updateScore() {
+    document.getElementById("pts").textContent = score;
+}
+
+function checkAchievements() {
+    if (score >= 100) {
+        document.getElementById("atc").style.visibility = "visible"
+        document.getElementById("pts").style.marginLeft = "0px"
+    }
+
+    if (score >= 700) {
+        document.getElementById("atc1").style.visibility = "visible"
+    }
+
+    if (score >= 1500) {
+        document.getElementById("atc2").style.visibility = "visible"
+    }
+
+    if (score >= 4000) {
+        document.getElementById("atc3").style.visibility = "visible"
+    }
+
+    if (score >= 8000) {
+        document.getElementById("atc4").style.visibility = "visible"
+    }
+
+    if (score >= 10000) {
+        document.getElementById("atc5").style.visibility = "visible"
+    }
+}
